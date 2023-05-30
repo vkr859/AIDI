1145d1c29acef816f980976ffad83dcd17bdaa91
Vishal <vkr859@gmail.com>
2023-05-30 17:46:42 -0400

    Final commit2

---
+++ Factorial.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = int(input("Enter a number: "))
+result = factorial(number)
+print("The factorial of", number, "is", result)
+
+
+# In[ ]:
+
+
+
+
